2a2fbeb46962dcc5d1787939f3c996d7f17ec3b8
ILYA <aleksin095@gmail.com>
2022-03-13 14:54:43 +0300

    Update code

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,17 @@
 import React from "react"
 import CTA from "./CTA"
-import "./header.css"
 import ME from "../../assets/me.png"
+import LOGO from "../../assets/logo_header.png"
 import HeaderSocials from "./HeaderSocials"
+import "./header.css"
 
 const Header = (props) => {
     return (
         <header>
             <div className="container header__container">
+                <a href="#" className="header__logo">
+                    <img src={LOGO} alt="Brand Logo" />
+                </a>
                 <h5>Hello, I'm</h5>
                 <h1>Ilya Aleksin</h1>
                 <h5 className="text-light">Web Developer</h5>
