46bb0d0fb02ec3890e399665f7841c8160e9bb65
ILYA <aleksin095@gmail.com>
2022-03-14 21:17:54 +0300

    update site

---
+++ src/components/portfolio/Data.jsx
@@ -0,0 +1,125 @@
+import IMG1 from "../../assets/project1.jpg"
+import IMG2 from "../../assets/project2.jpg"
+import IMG3 from "../../assets/project3.jpg"
+import IMG4 from "../../assets/project4.jpg"
+import IMG5 from "../../assets/project5.jpg"
+import IMG6 from "../../assets/project6.jpg"
+import IMG7 from "../../assets/project7.jpg"
+import IMG8 from "../../assets/project8.jpg"
+import IMG9 from "../../assets/project9.jpg"
+import IMG10 from "../../assets/project10.jpg"
+import IMG11 from "../../assets/project11.jpg"
+import IMG12 from "../../assets/project12.jpg"
+import IMG13 from "../../assets/project13.jpg"
+import IMG14 from "../../assets/project14.jpg"
+import IMG15 from "../../assets/project15.jpg"
+
+const data = [
+    {
+        id: 1,
+        image: IMG1,
+        title: "Afria-GeekBrains",
+        github: "https://github.com/jean-louis1776/afria-geekbrains",
+        demo: "https://afria-geekbrains.cf/",
+    },
+    {
+        id: 2,
+        image: IMG2,
+        title: "Guess My Number!",
+        github: "https://github.com/jean-louis1776/PROJECT-1-guess-my-number",
+        demo: "https://jean-louis1776.github.io/PROJECT-1-guess-my-number/",
+    },
+    {
+        id: 3,
+        image: IMG3,
+        title: "Pig Game",
+        github: "https://github.com/jean-louis1776/PROJECT-2-pig-game",
+        demo: "https://jean-louis1776.github.io/PROJECT-2-pig-game/",
+    },
+    {
+        id: 4,
+        image: IMG4,
+        title: "Calculator",
+        github: "https://github.com/jean-louis1776/PROJECT-3-calculator",
+        demo: "https://jean-louis1776.github.io/PROJECT-3-calculator/",
+    },
+    {
+        id: 5,
+        image: IMG5,
+        title: "JS-animation",
+        github: "https://github.com/jean-louis1776/PROJECT-4-JS-animation",
+        demo: "https://jean-louis1776.github.io/PROJECT-4-JS-animation/",
+    },
+    {
+        id: 6,
+        image: IMG6,
+        title: "Responsive Login Form",
+        github: "https://github.com/jean-louis1776/PROJECT-5-responsive-login-form",
+        demo: "https://jean-louis1776.github.io/PROJECT-5-responsive-login-form/",
+    },
+    {
+        id: 7,
+        image: IMG7,
+        title: "CSS Stacked Cards",
+        github: "https://github.com/jean-louis1776/PROJECT-6-Responsive-CSS-Stacked-Cards",
+        demo: "https://jean-louis1776.github.io/PROJECT-6-Responsive-CSS-Stacked-Cards/",
+    },
+    {
+        id: 8,
+        image: IMG8,
+        title: "Web-Store",
+        github: "https://github.com/jean-louis1776/web-store",
+        demo: "https://jean-louis1776.github.io/web-store/",
+    },
+    {
+        id: 9,
+        image: IMG9,
+        title: "Yummio (React App)",
+        github: "https://github.com/jean-louis1776/recipe-app-react",
+        demo: "https://yummio.netlify.app/",
+    },
+    {
+        id: 10,
+        image: IMG10,
+        title: "CSS Effect Finger Scan",
+        github: "https://github.com/jean-louis1776/PROJECT-7-CSS-Effect-Finger-Scan",
+        demo: "https://jean-louis1776.github.io/PROJECT-7-CSS-Effect-Finger-Scan/",
+    },
+    {
+        id: 11,
+        image: IMG11,
+        title: "Circular NavBar JS",
+        github: "https://github.com/jean-louis1776/PROJECT-9-Circular-NavBar-JS",
+        demo: "https://jean-louis1776.github.io/PROJECT-9-Circular-NavBar-JS/",
+    },
+    {
+        id: 12,
+        image: IMG12,
+        title: "CSS Effect Product Card",
+        github: "https://github.com/jean-louis1776/PROJECT-8-CSS-Effect-Product-Card",
+        demo: "https://jean-louis1776.github.io/PROJECT-8-CSS-Effect-Product-Card/",
+    },
+    {
+        id: 13,
+        image: IMG13,
+        title: "JavaScript Calendar",
+        github: "https://github.com/jean-louis1776/PROJECT-10-Full-Year-Calendar",
+        demo: "https://jean-louis1776.github.io/PROJECT-10-Full-Year-Calendar/",
+    },
+    {
+        id: 14,
+        image: IMG14,
+        title: "JavaScript Clock",
+        github: "https://github.com/jean-louis1776/PROJECT-11-JS-Clock",
+        demo: "https://jean-louis1776.github.io/PROJECT-11-JS-Clock/",
+    },
+    {
+        id: 15,
+        image: IMG15,
+        title: "Parallax Gallery",
+        github: "https://github.com/jean-louis1776/Parallax-Gallery",
+        demo: "https://jean-louis1776.github.io/Parallax-Gallery/",
+    },
+]
+
+export default data
